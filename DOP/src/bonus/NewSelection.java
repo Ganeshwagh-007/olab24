bbac12d22103f0acfe51ed2f74700c0f0dc8060c
Ganeshwagh-007 <ganeshwagh.1704@gmail.com>
2024-11-03 22:41:02 +0530

    Assignment 11 completed

--- DOP/src/bonus/NewSelection.java
+++ DOP/src/bonus/NewSelection.java
@@ -58,3 +58,58 @@ public class NewSelection {
         System.out.println("t1 vs t2: " + t1.compareTo(t2));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * package bonus;
+
+public sealed interface IBook<T> extends Comparable<IBook<T>> permits Comic, Fiction, TextBook {
+  
+    @Override
+    int compareTo(IBook<T> other);
+}
+enum FictionType {
+
+	Comedy,
+	Tragedy,
+	GoK
+}
+
+record Comic(String Title,int ageOfMainCharacter) implements IBook<Integer> {
+	
+	 @Override
+    public int compareTo(IBook<Integer> other) {
+        if (other instanceof Comic otherComic) {
+            return this.ageOfMainCharacter-otherComic.ageOfMainCharacter;
+        }
+        return 0; 
+    }
+
+}
+
+record Fiction(String name,FictionType type) implements IBook<FictionType> {
+
+	public int compareTo(IBook<FictionType> other) {
+        if (other instanceof Fiction otherFiction) {
+            return this.type.compareTo(otherFiction.type); 
+        }
+        return 0; 
+
+	}
+}
+
+ */
--- DOP/src/tests/OldSelectionTest.java
+++ DOP/src/tests/OldSelectionTest.java
@@ -1,18 +1,63 @@
 package tests;
 
+
 import org.junit.Assert;
 import oldshelf.*;
 import org.junit.Test;
 
 public class OldSelectionTest{
     @Test
+
     public void test1(){
         TextBook t = new TextBook("Politics");
         Assert.assertEquals(OldSelection.getAgeOrTitle(t),"Politics");
     }
     @Test
+
     public void test2(){
         Fiction f = new Fiction("Transformers",FictionType.GoK);
         Assert.assertEquals(OldSelection.getAgeOrTitle(f),"Age of Ultron");
     }
 }
+
+
+
+/*
+ * package bonus;
+
+public sealed interface IBook<T> extends Comparable<IBook<T>> permits Comic, Fiction, TextBook {
+  
+    @Override
+    int compareTo(IBook<T> other);
+}
+enum FictionType {
+
+	Comedy,
+	Tragedy,
+	GoK
+}
+
+record Comic(String Title,int ageOfMainCharacter) implements IBook<Integer> {
+	
+	 @Override
+    public int compareTo(IBook<Integer> other) {
+        if (other instanceof Comic otherComic) {
+            return this.ageOfMainCharacter-otherComic.ageOfMainCharacter;
+        }
+        return 0; 
+    }
+
+}
+
+record Fiction(String name,FictionType type) implements IBook<FictionType> {
+
+	public int compareTo(IBook<FictionType> other) {
+        if (other instanceof Fiction otherFiction) {
+            return this.type.compareTo(otherFiction.type); 
+        }
+        return 0; 
+
+	}
+}
+
+ */
